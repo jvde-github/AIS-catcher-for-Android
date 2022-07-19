0199986acfc282ae70e19f8821d1fca28840274b
jvde-github <jvde.github@gmail.com>
2022-07-19 20:10:59 +0200

    v0.16 added uses-feature android:name="android.hardware.usb.host" to Manifest

--- app/src/main/jni/JNI/AIScatcherNDK.cpp
+++ app/src/main/jni/JNI/AIScatcherNDK.cpp
@@ -237,7 +237,7 @@ Java_com_jvdegithub_aiscatcher_AisCatcherJava_InitNative(JNIEnv *env, jclass ins
     javaVersion = env->GetVersion();
     javaClass = (jclass) env->NewGlobalRef(instance);
 
-    callbackConsole(env, "AIS-Catcher " VERSION "-15\n");
+    callbackConsole(env, "AIS-Catcher " VERSION "-16\n");
     memset(&statistics, 0, sizeof(statistics));
 
     return 0;
