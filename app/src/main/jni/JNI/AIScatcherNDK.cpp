14d5dc34f8c78f3d56b021b9867c994c518f6383
jvde-github <jvde.github@gmail.com>
2022-09-04 20:30:28 +0200

    Update version codes

--- app/src/main/jni/JNI/AIScatcherNDK.cpp
+++ app/src/main/jni/JNI/AIScatcherNDK.cpp
@@ -268,7 +268,7 @@ Java_com_jvdegithub_aiscatcher_AisCatcherJava_InitNative(JNIEnv *env, jclass ins
     javaVersion = env->GetVersion();
     javaClass = (jclass) env->NewGlobalRef(instance);
 
-    callbackConsole(env, "AIS-Catcher " VERSION "-19\n");
+    callbackConsole(env, "AIS-Catcher " VERSION "-20\n");
     memset(&statistics, 0, sizeof(statistics));
 
     return 0;
